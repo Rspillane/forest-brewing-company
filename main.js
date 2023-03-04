396b96d7d1cd0c70e9a4c54ac9a882d8dc8854c6
Robert Spillane <robert.e.spillane@gmail.com>
2023-03-04 15:53:52 +0000

    navbar dropdown

--- main.js
+++ main.js
@@ -2,5 +2,12 @@
 const accordionContent = document.getElementsByClassName('accordion-content')[0];
 
 const expandAccordion = () => {
-    accordionContent.classList.toggle('active');
+    accordionContent.classList.toggle('open');
+}
+
+const navbar = document.getElementsByClassName('navbar-collapse')[0];
+
+
+const expandNavbar = () => {
+    navbar.classList.toggle('open');
 }
