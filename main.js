5384a0f73fc1027d8c30d8e2a3f21da5a38b8796
Robert Spillane <robert.e.spillane@gmail.com>
2023-03-04 14:44:08 +0000

    create accordian in footer

---
+++ main.js
@@ -0,0 +1,6 @@
+//const accordionBtn = document.getElementsByClassName('accordion-button')[0];
+const accordionContent = document.getElementsByClassName('accordion-content')[0];
+
+const expandAccordion = () => {
+    accordionContent.classList.toggle('active');
+}
